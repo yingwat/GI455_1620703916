72777a351a265fc38dd110687b3b6cc426b65cd5
LAPTOP-AU9A9TF4\Yingwat <>
2021-01-27 16:09:31 +0700

    Send Assignment Week1

---
+++ GI455_Assignment_Week1/Assets/Script/Find.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class Find : MonoBehaviour
+{
+    private string TextName;
+    public GameObject InputText;
+    public GameObject OutputText;
+    public Text TextShow;
+    public string[] TextData;
+
+    private void Start()
+    {
+        foreach (string textshow in TextData)
+        {
+            TextShow.text += textshow + "\n";
+        }
+    }
+
+    public void CheckTextData()
+    {
+        TextName = InputText.GetComponent<Text>().text;
+        
+            if (TextData.Contains(TextName))
+            {
+                OutputText.GetComponent<Text>().text = "[ " + "<color=green>" + TextName + "</color>" + " ]" + " is found.";
+            }
+            else
+            {
+                OutputText.GetComponent<Text>().text = "[ " + "<color=red>" +TextName + "</color>" + " ]" + " is not found.";
+            }
+        
+    }
+}
